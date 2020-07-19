dbdc8102275153db383cf6f2ae5382491c12735c
mariiavolkova <67053060+mariiavolkova@users.noreply.github.com>
2020-07-19 18:54:40 +0300

    Delete Main.java

--- src/main/java/com/volkova/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.volkova;
-
-import main.java.com.volkova.executor.Executor;
-
-public class Main {
-    public static void main(String[] args) {
-        new Executor().start();
-    }
-}
